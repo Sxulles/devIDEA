6308723982f063650c3cb4ef6554229f545d24cf
McKay <kg89szeged@gmail.com>
2023-09-12 11:53:19 +0200

    [ UPDATE ] Changed REACT to functional code and new background

--- devIDEA/ClientApp/src/components/FetchData.js
+++ devIDEA/ClientApp/src/components/FetchData.js
@@ -1,63 +1,56 @@
-import React, { Component } from 'react';
-import authService from './api-authorization/AuthorizeService'
+import React, { useEffect, useState } from 'react';
+import authService from './api-authorization/AuthorizeService';
 
-export class FetchData extends Component {
-  static displayName = FetchData.name;
+export const FetchData = () => {
+  const [forecasts, setForecasts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  constructor(props) {
-    super(props);
-    this.state = { forecasts: [], loading: true };
-  }
+  useEffect(() => {
+    const populateWeatherData = async () => {
+      const token = await authService.getAccessToken();
+      const response = await fetch('weatherforecast', {
+        headers: !token ? {} : { 'Authorization': `Bearer ${token}` }
+      });
+      const data = await response.json();
+      setForecasts(data);
+      setLoading(false);
+    };
 
-  componentDidMount() {
-    this.populateWeatherData();
-  }
+    populateWeatherData();
+  }, []);
 
-  static renderForecastsTable(forecasts) {
+  const renderForecastsTable = (forecasts) => {
     return (
-      <table className="table table-striped" aria-labelledby="tableLabel">
-        <thead>
+        <table className="table table-striped" aria-labelledby="tableLabel">
+          <thead>
           <tr>
             <th>Date</th>
             <th>Temp. (C)</th>
             <th>Temp. (F)</th>
             <th>Summary</th>
           </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+          {forecasts.map(forecast => (
+              <tr key={forecast.date}>
+                <td>{forecast.date}</td>
+                <td>{forecast.temperatureC}</td>
+                <td>{forecast.temperatureF}</td>
+                <td>{forecast.summary}</td>
+              </tr>
+          ))}
+          </tbody>
+        </table>
     );
-  }
+  };
 
-  render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : FetchData.renderForecastsTable(this.state.forecasts);
+  const contents = loading ? <p><em>Loading...</em></p> : renderForecastsTable(forecasts);
 
-    return (
+  return (
       <div>
         <h1 id="tableLabel">Weather forecast</h1>
         <p>This component demonstrates fetching data from the server.</p>
         {contents}
       </div>
-    );
-  }
-
-  async populateWeatherData() {
-    const token = await authService.getAccessToken();
-    const response = await fetch('weatherforecast', {
-      headers: !token ? {} : { 'Authorization': `Bearer ${token}` }
-    });
-    const data = await response.json();
-    this.setState({ forecasts: data, loading: false });
-  }
+  );
 }
--- devIDEA/ClientApp/src/components/NavMenu.js
+++ devIDEA/ClientApp/src/components/NavMenu.js
@@ -1,34 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LoginMenu } from './api-authorization/LoginMenu';
 import './NavMenu.css';
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+export const NavMenu = () => {
+  const [collapsed, setCollapsed] = useState(true);
 
-  constructor (props) {
-    super(props);
+  const toggleNavbar = () => {
+    setCollapsed(!collapsed);
+  };
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render() {
-    return (
+  return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
           <NavbarBrand tag={Link} to="/">devIDEA</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+          <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
             <ul className="navbar-nav flex-grow">
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
@@ -39,12 +27,10 @@ export class NavMenu extends Component {
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
               </NavItem>
-              <LoginMenu>
-              </LoginMenu>
+              <LoginMenu />
             </ul>
           </Collapse>
         </Navbar>
       </header>
-    );
-  }
+  );
 }
