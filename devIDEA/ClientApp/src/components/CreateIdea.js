1be7cdbb1b5b18afd91165c693267a4a4929df72
McKay <kg89szeged@gmail.com>
2023-09-14 13:52:18 +0200

    [ UPDATE ] The connection between client and server works fine

--- devIDEA/ClientApp/src/components/CreateIdea.js
+++ devIDEA/ClientApp/src/components/CreateIdea.js
@@ -11,20 +11,17 @@ export const CreateIdea = () => {
     const [defaultState4, setDefaultState4] = useState("default");
     const [defaultState5, setDefaultState5] = useState("default");
     const [defaultState6, setDefaultState6] = useState("default");
-    const questions = [];
+    const [questions, setQuestions] = useState({});
 
     useEffect(() => {
-        populateQuestions();
-        createQuestions();
+        const fetch = populateQuestions();
     }, []);
 
     const populateQuestions = async () => {
         try {
             const response = await fetch('https://localhost:7048/selectGetQuestions');
             const data = await response.json();
-            data.forEach((e) => {
-                questions.push(e);
-            })
+            setQuestions(data);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -112,74 +109,76 @@ export const CreateIdea = () => {
             <h3>Make your idea reality</h3>
 
             <div className="question-contaier" style={currentQuestion === 0 ? {display : "block"} : {display : "block"}}>
-                <span id="question1"></span>
+                <span>{questions[0]}</span>
                 <select value={defaultState1} className="select1" onChange={handleOnChange} data-value="question1">
-                    <option value="default" >Please choose</option>
-                    <option value="option2" >option 2</option>
-                    <option value="option3" >option 3</option>
-                    <option value="option4" >option 4</option>
-                    <option value="option5" >option 5</option>
-                    <option value="option6" >option 6</option>
+                    <option value="default">Please choose</option>
+                    <option value="option2">{questions && questions[0][0]}</option>
+                    <option value="option3">{questions[0][1]}</option>
+                    <option value="option4">{questions[0][2]}</option>
+                    <option value="option5">{questions[0][3]}</option>
+                    <option value="option6">{questions[0][4]}</option>
+                    <option value="option6">{questions[0][5]}</option>
                 </select>
             </div>
 
             <div className="question-contaier"  style={currentQuestion >= 1 ? {display : "block"} : {display : "none"}}>
-                <span id="question1"></span>
+                <span>{questions[1]}</span>
                 <select value={defaultState2} className="select2" onChange={handleOnChange}  data-value="question2">
                     <option value="default">Please choose</option>
-                    <option value="option1">option 2</option>
-                    <option value="option2">option 3</option>
-                    <option value="option3">option 4</option>
-                    <option value="option4">option 5</option>
-                    <option value="option5">option 6</option>
+                    <option value="option1">{questions[1][0]}</option>
+                    <option value="option2">{questions[1][1]}</option>
+                    <option value="option3">{questions[1][2]}</option>
+                    <option value="option4">{questions[1][3]}</option>
+                    <option value="option5">{questions[1][4]}</option>
+                    <option value="option5">{questions[1][5]}</option>
                 </select>
             </div>
 
             <div className="question-contaier" style={currentQuestion >= 2 ? {display : "block"} : {display : "none"}}>
-                <span id="question1"></span>
+                <span>{questions[2]}</span>
                 <select value={defaultState3} className="select3" onChange={handleOnChange}  data-value="question3">
                     <option value="default">Please choose</option>
-                    <option value="option1">option 2</option>
-                    <option value="option2">option 3</option>
-                    <option value="option3">option 4</option>
-                    <option value="option4">option 5</option>
-                    <option value="option5">option 6</option>
+                    <option value="option1">{questions[2][0]}</option>
+                    <option value="option2">{questions[2][1]}</option>
+                    <option value="option3">{questions[2][2]}</option>
+                    <option value="option4">{questions[2][3]}</option>
+                    <option value="option5">{questions[2][4]}</option>
                 </select>
             </div>
 
             <div className="question-contaier" style={currentQuestion >= 3 ? {display : "block"} : {display : "none"}}>
-                <span id="question1"></span>
+                <span>{questions[3]}</span>
                 <select value={defaultState4} className="select4" onChange={handleOnChange}  data-value="question4">
                     <option value="default">Please choose</option>
-                    <option value="option1">option 2</option>
-                    <option value="option2">option 3</option>
-                    <option value="option3">option 4</option>
-                    <option value="option4">option 5</option>
-                    <option value="option5">option 6</option>
+                    <option value="option1">{questions[3][0]}</option>
+                    <option value="option2">{questions[3][1]}</option>
+                    <option value="option3">{questions[3][2]}</option>
+                    <option value="option4">{questions[3][3]}</option>
+                    <option value="option5">{questions[3][4]}</option>
+                    <option value="option5">{questions[3][5]}</option>
                 </select>
             </div>
 
             <div className="question-contaier" style={currentQuestion >= 4 ? {display : "block"} : {display : "none"}}>
-                <span id="question1"></span>
+                <span>{questions[4]}</span>
                 <select value={defaultState5} className="select5" onChange={handleOnChange}  data-value="question5">
                     <option value="default">Please choose</option>
-                    <option value="option1">option 2</option>
-                    <option value="option2">option 3</option>
-                    <option value="option3">option 4</option>
-                    <option value="option4">option 5</option>
-                    <option value="option5">option 6</option>
+                    <option value="option1">{questions[4][0]}</option>
+                    <option value="option2">{questions[4][1]}</option>
+                    <option value="option3">{questions[4][2]}</option>
+                    <option value="option4">{questions[4][3]}</option>
                 </select>
             </div>
 
             <div className="question-contaier" style={currentQuestion >= 5 ? {display : "block"} : {display : "none"}}>
-                <span>Here is the question 6!</span>
+                <span>{questions[5]}</span>
                 <select value={defaultState6} className="select6" onChange={handleOnChange}  data-value="question6">
                     <option value="default">Please choose</option>
-                    <option value="option1">option 2</option>
-                    <option value="option2">option 3</option>
-                    <option value="option3">option 4</option>
-                    <option value="option4">option 5</option>
-                    <option value="option5">option 6</option>
+                    <option value="option1">{questions[5][0]}</option>
+                    <option value="option2">{questions[5][1]}</option>
+                    <option value="option3">{questions[5][2]}</option>
+                    <option value="option4">{questions[5][3]}</option>
+                    <option value="option5">{questions[5][4]}</option>
                 </select>
             </div>
 
