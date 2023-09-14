21a20dbd63e3ea80d0704f08ae538cc393d99411
Sxulles <seres1322@gmail.com>
2023-09-14 22:53:04 +0200

    [ UPDATE ] updated hero UI & fixed navbar

--- devIDEA/ClientApp/src/components/Hero.js
+++ devIDEA/ClientApp/src/components/Hero.js
@@ -7,17 +7,20 @@ import sqlLogo from "../assets/img/sql_logo.png"
 import htmlLogo from "../assets/img/html_logo.png"
 export const Hero = () => {
     return (
-        <div className="bg-purple-gradient">
-                <div className="container-fluid pt-5 mt-5 pb-5">
+        <div className="hero">
+                <div className="container-fluid pt-5 pb-5">
 
-                    <div className="mb-5 mb-lg-2 mx-auto text-center bg-gradient-custom">
-                        <div className="mb-5">
-                            <div className="mb-3">
-                                <h2 className="mb-3 fs-1 text-light display-1">Develop your <span className="text-purpleblue fw-bold">IDEA</span> through us!</h2>
+                    <div className="mb-5 mb-lg-2 mx-auto text-center max-w-55 py-4 mt-3">
+                        <div className="mb-5 mt-5">
+                            <div className="mb-4">
+                                <h2 className="fs-1 text-light display-1 text-shadow-sm">
+                                    <span className="text-shadow-sm">Develop your </span>
+                                    <span className="text-shadow-sm px-4 border bg-dark-transparent text-white">IDEA</span>
+                                    <span className="text-shadow-sm"> through us!</span></h2>
                             </div>
                             
                             
-                            <p className="lead text-white-transparent mb-0">
+                            <p className="lead text-white mb-0">
                                 devIDEA hands you a direct and easy way to assemble your idea, to a viable product frame,
                                 which with you can start the development proccess literally.
                             </p>
@@ -31,20 +34,17 @@ export const Hero = () => {
                         </div>
                     </div>
                 </div>
-            
-                <div className="container-fluid mt-5 bg-dark-transparent">
-                    <div className="container">
-                        <div className="row align-items-center">
-                            <div className="col"><img className="img-fluid h-25 p-5" src={vsLogo}/></div>
-                            <div className="col"><img className="img-fluid h-25 p-5" src={jsLogo}/></div>
-                            <div className="col"><img className="img-fluid h-25 p-5" src={dotnetLogo}/></div>
-                            <div className="col"><img className="img-fluid h-25 p-5" src={csharplogo}/></div>
-                            <div className="col"><img className="img-fluid h-25 p-5" src={cssLogo}/></div>
-                            <div className="col"><img className="img-fluid h-25 p-5" src={sqlLogo}/></div>
-                            <div className="col"><img className="img-fluid h-25 p-5" src={htmlLogo}/></div>
-                        </div>
-                    </div>
+
+            <div className="container-fluid mt-5">
+                <div className="row align-items-center">
+                    <div className="col-12 col-md-6 col-lg-2 p-3"><img className="img-fluid d-block mx-auto h-custom" src={vsLogo}/></div>
+                    <div className="col-12 col-md-6 col-lg-2 p-3"><img className="img-fluid d-block mx-auto h-custom" src={jsLogo}/></div>
+                    <div className="col-12 col-md-6 col-lg-2 p-3"><img className="img-fluid d-block mx-auto h-custom" src={dotnetLogo}/></div>
+                    <div className="col-12 col-md-6 col-lg-2 p-3"><img className="img-fluid d-block mx-auto h-custom" src={csharplogo}/></div>
+                    <div className="col-12 col-md-6 col-lg-2 p-3"><img className="img-fluid d-block mx-auto h-custom" src={cssLogo}/></div>
+                    <div className="col-12 col-md-6 col-lg-2 p-3"><img className="img-fluid d-block mx-auto h-custom" src={sqlLogo}/></div>
                 </div>
+            </div>
         </div>
     );
 }
